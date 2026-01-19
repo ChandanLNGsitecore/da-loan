638f307484b205df3d883f9bbd1914556e2d92ee
chandanlngsitecore <chandan.kumar@lng-consultancy.com>
2026-01-19 17:27:41 +0530

    css fixed

--- FRB/DACorporateWeb/src/app/layout.tsx
+++ FRB/DACorporateWeb/src/app/layout.tsx
@@ -1,20 +1,46 @@
-import { draftMode } from 'next/headers';
-import Bootstrap from 'src/Bootstrap';
+import "./globals.css";
+import { Montserrat } from "next/font/google";
+import localFont from "next/font/local";
 
-export default async function SiteLayout({
+// Configure Montserrat font
+const montserrat = Montserrat({
+  variable: "--font-montserrat",
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700", "800"],
+  display: "swap",
+});
+
+// Configure Nohemi local font
+const nohemi = localFont({
+  src: [
+    {
+      path: "../assets/public/fonts/Nohemi-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../assets/public/fonts/Nohemi-Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../assets/public/fonts/Nohemi-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+  variable: "--font-nohemi",
+  display: "swap",
+});
+
+export default function RootLayout({
   children,
-  params,
 }: {
   children: React.ReactNode;
-  params: Promise<{ site: string }>;
 }) {
-  const { site } = await params;
-  const { isEnabled } = await draftMode();
-
   return (
-    <>
-      <Bootstrap siteName={site} isPreviewMode={isEnabled} />
-      {children}
-    </>
+    <html lang="en" className={`${montserrat.variable} ${nohemi.variable}`}>
+      <body>{children}</body>
+    </html>
   );
 }
